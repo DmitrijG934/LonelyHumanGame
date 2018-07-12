094e2f90b1a425ed37d2162907a9c27af1710cfd
Dmitryi Gordeev <dmitrijg934@gmail.com>
2018-07-12 20:26:15 +0300

    Initial commit

---
+++ desktop/src/com/dmitrijg/game/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.dmitrijg.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dmitrijg.game.LonelyHuman;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		System.setProperty("user.name", "user");
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LonelyHuman(), config);
+	}
+}
